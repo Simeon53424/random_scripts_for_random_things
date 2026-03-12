f2d08877093caa56d2dd9e076b6110b6d4410471
Simeon YTRoot <simeon.ytroot@gmail.com>
2026-03-12 22:34:30 +0100

    Refactor duration calculation variables and improve output formatting

--- fritz.box - internal/dial-total_calls_of_name.js
+++ fritz.box - internal/dial-total_calls_of_name.js
@@ -1,24 +1,34 @@
 // INPUT VARIABLES
 name_number = "" // (type: string) specifies Name/Number to search for
-
 // INPUT VARIABLES
 
-d = 0
-h = 0
-m = 0
-c = 0
+days = 0
+hours = 0
+minutes = 0
+callCount = 0
 for (let i of Array.from(document.getElementsByClassName("duration")).slice(1)) {
-    if (i.parentElement.children[2].title==name_number) {
-        if (i.getAttribute('data-timestr') != '0:00') {
-            h += Number(i.getAttribute('data-timestr').split(':')[0])
-            m += Number(i.getAttribute('data-timestr').split(':')[1])
-            console.log(`[${i.parentElement.children[0].getAttribute('datalabel')}]: ${i.getAttribute('data-timestr')}`)
-            c += 1
+    if (i.parentElement.children[2].title == name_number || i.parentElement.children[2].innerText == name_number) {
+        const timeStr = i.getAttribute('data-timestr')
+        if (timeStr != '0:00') {
+            const timeParts = timeStr.split(':')
+            hours += Number(timeParts[0])
+            minutes += Number(timeParts[1])
+            console.log(`[${i.parentElement.children[0].getAttribute('datalabel')}]: ${timeStr}`)
+            callCount += 1
         }
     }
 }
-h += Math.floor(m / 60)
-m -= (60 * Math.floor(m / 60))
-d += Math.floor(h / 24)
-h -= (24 * Math.floor(h / 24))
-console.log(`Total: \n  - Calls: ${c}\n  - Duration: ${d}:${h}:${m}`)
+hours += Math.floor(minutes / 60)
+minutes -= (60 * Math.floor(minutes / 60))
+days += Math.floor(hours / 24)
+hours -= (24 * Math.floor(hours / 24))
+
+finalOutputDuration = ""
+if (days > 0) {
+    finalOutputDuration += `${days.toString().padStart(2, '0')}d:`
+}
+if (hours > 0) {
+    finalOutputDuration += `${hours.toString().padStart(2, '0')}h:`
+}
+finalOutputDuration += `${minutes.toString().padStart(2, '0')}m`
+console.log(`Total: \n  - Calls: ${callCount}\n  - Duration: ${finalOutputDuration}`)
