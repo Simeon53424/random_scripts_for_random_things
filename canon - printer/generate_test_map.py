99a5e251fffae01cab0d90c5acb783835a966f1e
Simeon YTRoot <simeon.ytroot@gmail.com>
2026-03-16 23:05:25 +0100

    FIX: 1. attempt on fixing the border

--- canon - printer/generate_test_map.py
+++ canon - printer/generate_test_map.py
@@ -12,5 +12,5 @@ def cm_to_pt(cm):
 c = canvas.Canvas("map.pdf", pagesize=A4)
 c.setFillColorRGB(0.0, 0.0, 0.0)
 c.setLineWidth(0.1)
-c.rect(0, 0, width, height, stroke=1, fill=0)
+c.rect(1, 1, width-2, height-2, stroke=1, fill=0)
 c.save()
